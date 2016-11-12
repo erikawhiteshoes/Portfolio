fbb1585e9e643c64f1b8a22b341923dc1c5dd7fc
erikawhiteshoes <erikawhiteshoes@gmail.com>
2016-11-12 11:07:19 -0500

    Updated themes and colors

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,11 @@ gulp.task('static', function() {
             .pipe(gulp.dest('dist/static'));
 });
 
+gulp.task('vendor', function() {
+  return gulp.src('src/vendor/**/*')
+            .pipe(gulp.dest('dist/vendor'));
+});
+
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('src/less/creative.less')
@@ -87,31 +92,31 @@ gulp.task('minify-js', function() {
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['src/node_modules/bootstrap/dist/**/*', '!src/**/npm.js', '!src/**/bootstrap-theme.*', '!src/**/*.map'])
-        .pipe(gulp.dest('dist/vendor/bootstrap'))
+// gulp.task('copy', function() {
+//     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+//         .pipe(gulp.dest('dist/vendor/bootstrap'))
+//
+//     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+//         .pipe(gulp.dest('dist/vendor/jquery'))
+//
+//     gulp.src(['node_modules/magnific-popup/dist/*'])
+//         .pipe(gulp.dest('dist/vendor/magnific-popup'))
+//
+//     gulp.src(['node_modules/scrollreveal/dist/*.js'])
+//         .pipe(gulp.dest('dist/vendor/scrollreveal'))
+//
+//     gulp.src([
+//             'node_modules/font-awesome/**',
+//             '!node_modules/font-awesome/**/*.map',
+//             '!node_modules/font-awesome/.npmignore',
+//             '!node_modules/font-awesome/*.txt',
+//             '!node_modules/font-awesome/*.md',
+//             '!node_modules/font-awesome/*.json'
+//         ])
+//         .pipe(gulp.dest('dist/vendor/font-awesome'))
+// })
 
-    gulp.src(['src/node_modules/jquery/dist/jquery.js', 'src/node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('dist/vendor/jquery'))
-
-    gulp.src(['src/node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('dist/vendor/magnific-popup'))
-
-    gulp.src(['src/node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('dist/vendor/scrollreveal'))
-
-    gulp.src([
-            'src/node_modules/font-awesome/**',
-            '!src/node_modules/font-awesome/**/*.map',
-            '!src/node_modules/font-awesome/.npmignore',
-            '!src/node_modules/font-awesome/*.txt',
-            '!src/node_modules/font-awesome/*.md',
-            '!src/node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('src/dist/vendor/font-awesome'))
-})
-
-gulp.task('default', ['pages', 'static', 'images', 'less', 'minify-css', 'minify-js', 'copy' ]);
+gulp.task('default', ['pages', 'static', 'vendor', 'images', 'less', 'minify-css', 'minify-js']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
